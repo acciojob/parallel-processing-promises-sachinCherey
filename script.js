7c8e0837aae79f89d84fe394a2ccb28bdea9e6ae
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-08-17 16:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+    const imageUrls = [
+            { url: 'url_of_image_1.jpg' },
+            { url: 'url_of_image_2.jpg' },
+            // ... Add more image URLs here
+        ];
+
+        function downloadImage(image) {
+            return new Promise((resolve, reject) => {
+                const imgElement = new Image();
+                imgElement.onload = () => resolve(imgElement);
+                imgElement.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+                imgElement.src = image.url;
+            });
+        }
+
+        function displayImages(images) {
+            const outputDiv = document.getElementById('output');
+            images.forEach(imageElement => {
+                outputDiv.appendChild(imageElement);
+            });
+        }
+
+        document.getElementById('download-images-button').addEventListener('click', () => {
+            const downloadPromises = imageUrls.map(downloadImage);
+
+            Promise.all(downloadPromises)
+                .then(displayImages)
+                .catch(error => console.error(error));
+        });
